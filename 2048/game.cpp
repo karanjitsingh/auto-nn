a28c59072ac302bacdc91a33390d40ffb0df8c15
Karan Jit Singh <karan1866@gmail.com>
2017-09-26 12:42:22 +0530

    started with 2048

--- 2048/game.cpp
+++ 2048/game.cpp
@@ -4,32 +4,77 @@ using namespace std;
 
 namespace Two048 {
 
-    struct state GridState() {
-        bool full,
-        bool valid
-    } state;
+    struct GameState {
+        bool full;          // Grid is full, can't add another random value
+        bool valid;         // Move resulting in no change to grid
+        bool halt;          // Game can't proceed further
+        int score;
+    };
+
+    enum Direction{Up, Down, Left, Right};
+
+
 
     class Game {
-        
+
     private:
+
+        // Static constructor
+        struct staticConstructor {
+        public:
+            staticConstructor() {
+                srand(time(NULL));
+                cout << "wtf";
+            }
+        };
+
         int ** grid;
         int score;
-        
-        
+        int size;
+        GameState currState;
+
+        int addRandom() {
+            return 1;
+        }
+
     public:
-        
-        static enum Direction{Up, Down, Left, Right};
-        
+
         Game(int n) {
-            this.score = 0;
-            this.grid = new int*[n];
+            // Call to static constructor, will be called only once
+            static staticConstructor cons;
+
+            size = n;
+            currState.score = 0;
+            grid = new int*[n];
             for(int i=0;i<n;i++) {
                 grid[i] = new int[n];
             }
-            
-            this.state.full = false;
-            this.state.valid = true;
+
+            score = 0;
+
+            addRandom();
+            addRandom();
         }
+
+        GameState transition(Direction d) {
+
+            switch(d) {
+                case Up:
+                    break;
+                case Down:
+                    break;
+                case Left:
+                    break;
+                case Right:
+                    break;
+                default:
+                    throw "Invalid direction.";
+            }
+
+            return this->currState;
+        };
         
-    }
+    };
+
+
 }
--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
 #include <iostream>
+#include "2048/game.cpp"
+
+using namespace std;
+
+
+int main() {
+
+    Two048::Game game(3);
+    Two048::Game game2(3);
+
+
+    game.transition(Two048::Up);
+    cout << "bullshit";
+    return 0;
+}
